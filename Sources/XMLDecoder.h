3a94df93a12495584a6d1c680875e5a95a9d8a81
Yuki Nagai <ynagai@r.recruit.co.jp>
2018-01-01 17:41:17 +0900

    travis

---
+++ Sources/XMLDecoder.h
@@ -0,0 +1,18 @@
+//
+//  XMLDecoder.h
+//  XMLDecoder
+//
+//  Created by Yuki Nagai on 2017/12/17.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XMLDecoder.
+FOUNDATION_EXPORT double XMLDecoderVersionNumber;
+
+//! Project version string for XMLDecoder.
+FOUNDATION_EXPORT const unsigned char XMLDecoderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XMLDecoder/PublicHeader.h>
+
+
